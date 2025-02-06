3a22dedc6e131b6bad63af7e5bac97af2b2b7c14
KasiaS999 <ksuch999@gmail.com>
2025-02-06 10:00:54 +0100

    VFS-12118 replace lager with logger

--- src/cluster_manager.erl
+++ src/cluster_manager.erl
@@ -39,6 +39,7 @@
     {ok, pid(), State :: term()} |
     {error, Reason :: term()}).
 start(_StartType, _StartArgs) ->
+    onedata_logger:configure_logger(),
     test_node_starter:maybe_start_cover(),
     cluster_manager_sup:start_link().
 
